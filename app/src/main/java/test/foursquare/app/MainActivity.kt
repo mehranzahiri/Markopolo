f83c340742c66aa84c937814351f498c3881bdd2
Mehran  zahiri <mehran.zahiri.eng@gmail.com>
2019-10-17 11:22:35 +0330

    Initialize project

---
+++ app/src/main/java/test/foursquare/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package test.foursquare.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
