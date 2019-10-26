80edeae0e4fefcf6a0652bb1157837911c765367
Mehran  zahiri <mehran.zahiri.eng@gmail.com>
2019-10-26 08:47:09 +0330

    Binding data in venue detail activity for show more info

--- app/src/main/java/test/foursquare/app/model/structures/VenueDetailStruct.kt
+++ app/src/main/java/test/foursquare/app/model/structures/VenueDetailStruct.kt
@@ -1,12 +1,13 @@
 package test.foursquare.app.model.structures
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "venueDetails")
 data class VenueDetailStruct(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val detail_id: Int,
     val venue_id: String,
     val photo: String?,
     val hours: String?,
@@ -14,5 +15,7 @@ data class VenueDetailStruct(
     val like: String,
     val dislike: String?,
     val rating: Double?,
-    val ratingColor: String?
+    val ratingColor: String?,
+    @Embedded
+    val venueStruct: VenueStruct
 )
--- app/src/main/java/test/foursquare/app/ui/venueDetail/VenueDetailActivity.kt
+++ app/src/main/java/test/foursquare/app/ui/venueDetail/VenueDetailActivity.kt
@@ -1,17 +1,17 @@
 package test.foursquare.app.ui.venueDetail
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
 import test.foursquare.app.R
+import test.foursquare.app.databinding.ActivityVenueDetailBinding
 import test.foursquare.app.utilities.Consts
 import test.foursquare.app.utilities.Coroutines
-import test.foursquare.app.utilities.GlobalActivity
 
 class VenueDetailActivity : AppCompatActivity(), KodeinAware {
 
@@ -19,6 +19,7 @@ class VenueDetailActivity : AppCompatActivity(), KodeinAware {
     private val factory: VenueDetailViewModelFactory by instance()
     private lateinit var viewModel: VenueDetailViewModel
     private lateinit var VENUE_ID: String
+    private lateinit var dataBinding: ActivityVenueDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_venue_detail)
@@ -26,13 +27,17 @@ class VenueDetailActivity : AppCompatActivity(), KodeinAware {
         VENUE_ID = intent.getStringExtra(Consts.VENUE_ID_KEY)!!
         viewModel = ViewModelProviders.of(this, factory).get(VenueDetailViewModel::class.java)
 
+        dataBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_venue_detail)
         Coroutines.main {
             viewModel.getVenueDetail(VENUE_ID).observe(this, Observer { venueDetail ->
                 Coroutines.main {
-
+                    dataBinding.venueDetailStruct = venueDetail
                 }
 
             })
         }
     }
+
+
 }
